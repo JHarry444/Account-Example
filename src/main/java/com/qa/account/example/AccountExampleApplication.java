cb9392b8a248d5971754c8515770f6cdad1b76f4
jharry444 <jordan.harrison@qa.com>
2019-09-17 17:36:37 +0100

    initial project set up

---
+++ src/main/java/com/qa/account/example/AccountExampleApplication.java
@@ -0,0 +1,13 @@
+package com.qa.account.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/account/example/AccountExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qa.account.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
